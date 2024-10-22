92d6cf8b9a0e91f27eef203d34eb4995f495cff8
Devon Kiss <faktsonly@gmail.com>
2024-10-22 01:50:22 -0500

    Removed illegal ARIA role identifier 'content' from the homepage. I'm finally learning these so I am trying to get it right.

--- app/page.js
+++ app/page.js
@@ -1,15 +1,13 @@
 export default function Home() {
   return (
     <>
-      <section role="content">
-        <div className="my-24">
-          <h1 className="text-4xl font-bold text-center">3D MACHINE AND TOOL LLC</h1>
+      <div className="my-24">
+        <h1 className="text-4xl font-bold text-center">3D MACHINE AND TOOL LLC</h1>
 
-          <div className="products_link w-full text-center">
-            <a href="/products" className="underline text-blue-400">Our Products</a>
-          </div>
+        <div className="products_link w-full text-center">
+          <a href="/products" className="underline text-blue-400">Our Products</a>
         </div>
-      </section>
+      </div>
     </>
   );
 }
