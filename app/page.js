7f52254a585b12bfc9c716ad5a4766c284e60d65
Devon Kiss <faktsonly@gmail.com>
2024-10-24 03:28:29 -0500

    Insertion of a Separator into the global component set to carefully divide separate content blocks from each other.

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import Seperator from '@/components/global/Seperator';
 import Hero from '@/components/Hero'
 
 export default function Home() {
@@ -5,6 +6,8 @@ export default function Home() {
     <>
       <div className="my-24 mx-auto">
         <Hero />
+
+        <Seperator />
       </div>
     </>
   );
---
+++ components/global/Seperator.js
@@ -0,0 +1,11 @@
+const Seperator = () => {
+  return (
+    <>
+      <hr
+        className="w-[45%] border-[#9ca3af44] mx-auto md:w-[55%] mb-96 hover:border-white duration-700"
+      />
+    </>
+  );
+}
+
+export default Seperator;
