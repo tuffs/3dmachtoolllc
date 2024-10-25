cee89d908ac9d300b068175c78306f8765488b3a
Devon Kiss <faktsonly@gmail.com>
2024-10-25 01:20:25 -0500

    Added SpeedInsights component from Vercel to the app.

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css";
 import Navbar from "@/components/global/Navbar";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata = {
   title: "3D MACHINE + TOOL LLC",
@@ -11,6 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <>
       <Analytics/>
+      <SpeedInsights/>
       <html lang="en">
         <body
           className={`antialiased`}
