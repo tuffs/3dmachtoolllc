fa7768dff24023e3c39fe263ad423eaeed22b4a5
Devon Kiss <faktsonly@gmail.com>
2024-10-25 05:44:56 -0500

    Changes in <RequestForQuote/> component and updates to remove the Capabilities link as it will be provided in the Services page.

--- data/links.js
+++ data/links.js
@@ -4,7 +4,6 @@ const links = [
   { href: '/portfolio', label: 'PORTFOLIO' },
   { href: '/products', label: 'OUR PRODUCTS' },
   { href: '/services', label: 'SERVICES' },
-  { href: '/capabilities', label: 'CAPABILITIES' },
   { href: '/request-for-quote', label: 'REQUEST FOR QUOTE' },
   { href: '/contact-us', label: 'CONTACT US' },
 ];
