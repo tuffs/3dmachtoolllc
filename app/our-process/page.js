10bbf76bae15b4c0dd5c89b7d6025c2a1ab7ca1c
Devon Kiss <faktsonly@gmail.com>
2024-10-31 08:24:11 -0500

    Updates to the processes used in the our-process/page.js, the data comes from @/data/processes.js now, prior to database association for mocked data.

--- app/our-process/page.js
+++ app/our-process/page.js
@@ -1,44 +1,51 @@
 import Hero from '@/components/Hero';
 import ProcessCards from '@/components/ProcessCards';
+import processes from '@/data/processes';
 
-const processes = [
-  {
-    id: 1,
-    heading: 'DESIGN DOCUMENTS',
-    content: 'Project requirements, design documents, CAD, and 3D Model files are provided to us to analyzed to ensure feasibility of client\'s needs.',
-    imageUrl: '/process/01_design_documents.webp',
-  },
-  {
-    id: 2,
-    heading: 'QUOTE',
-    content: 'After we have received the requirements and they are properly analyzed, we provide a detailed quotation that includes the project scope, budget, and timeline for completion.',
-    imageUrl: '/process/02_quote.webp',
-  },
-  {
-    id: 3,
-    heading: 'ACCEPTANCE',
-    content: 'Upon project acceptance, we engage with our client and begin the project. Agreed upon payments are received, materials are procured, and the project is officially initiated.',
-    imageUrl: '/process/03_acceptance.webp',
-  },
-  {
-    id: 4,
-    heading: 'PRODUCTION',
-    content: 'Our team of experts begin the manufacturing process. This includes precision machining and assembly according to the design documents, requirements and agreed upon contract details.',
-    imageUrl: '/process/04_production.webp',
-  },
-  {
-    id: 5,
-    heading: 'QUALITY CONTROL',
-    content: 'The quality control and assurance process is conducted after completion of each part to ensure the project meets the tolerance and quality requirements. This includes a final inspection and testing.',
-    imageUrl: '/process/05_quality_control.webp',
-  },
-  {
-    id: 6,
-    heading: 'SHIPMENT',
-    content: 'Custom designed packaging is arranged for each completed manufacturing run to ensure that the your project is delivered safe and secure to your location. Photos and tracking information are always provided. ',
-    imageUrl: '/process/06_shipment.webp',
-  },
-];
+// const processes = [
+//   {
+//     id: 1,
+//     heading: 'DESIGN DOCUMENTS',
+//     content: 'Project requirements, design documents, CAD, and 3D Model files are provided to us to analyzed to ensure feasibility of client\'s needs.',
+//     imageUrl: '/process/01_design_documents.webp',
+//     longContent: '',
+//   },
+//   {
+//     id: 2,
+//     heading: 'QUOTE',
+//     content: 'After we have received the requirements and they are properly analyzed, we provide a detailed quotation that includes the project scope, budget, and timeline for completion.',
+//     imageUrl: '/process/02_quote.webp',
+//     longContent: '',
+//   },
+//   {
+//     id: 3,
+//     heading: 'ACCEPTANCE',
+//     content: 'Upon project acceptance, we engage with our client and begin the project. Agreed upon payments are received, materials are procured, and the project is officially initiated.',
+//     imageUrl: '/process/03_acceptance.webp',
+//     longContent: '',
+//   },
+//   {
+//     id: 4,
+//     heading: 'PRODUCTION',
+//     content: 'Our team of experts begin the manufacturing process. This includes precision machining and assembly according to the design documents, requirements and agreed upon contract details.',
+//     imageUrl: '/process/04_production.webp',
+//     longContent: '',
+//   },
+//   {
+//     id: 5,
+//     heading: 'QUALITY CONTROL',
+//     content: 'The quality control and assurance process is conducted after completion of each part to ensure the project meets the tolerance and quality requirements. This includes a final inspection and testing.',
+//     imageUrl: '/process/05_quality_control.webp',
+//     longContent: '',
+//   },
+//   {
+//     id: 6,
+//     heading: 'SHIPMENT',
+//     content: 'Custom designed packaging is arranged for each completed manufacturing run to ensure that the your project is delivered safe and secure to your location. Photos and tracking information are always provided. ',
+//     imageUrl: '/process/06_shipment.webp',
+//     longContent: '',
+//   },
+// ];
 
 
 export default function OurProcessPage() {
---
+++ data/processes.js
@@ -0,0 +1,46 @@
+const processes = [
+  {
+    id: 1,
+    heading: 'DESIGN DOCUMENTS',
+    content: 'Project requirements, design documents, CAD, and 3D Model files are provided to us to analyzed to ensure feasibility of client\'s needs.',
+    imageUrl: '/process/01_design_documents.webp',
+    longContent: '',
+  },
+  {
+    id: 2,
+    heading: 'QUOTE',
+    content: 'After we have received the requirements and they are properly analyzed, we provide a detailed quotation that includes the project scope, budget, and timeline for completion.',
+    imageUrl: '/process/02_quote.webp',
+    longContent: '',
+  },
+  {
+    id: 3,
+    heading: 'ACCEPTANCE',
+    content: 'Upon project acceptance, we engage with our client and begin the project. Agreed upon payments are received, materials are procured, and the project is officially initiated.',
+    imageUrl: '/process/03_acceptance.webp',
+    longContent: '',
+  },
+  {
+    id: 4,
+    heading: 'PRODUCTION',
+    content: 'Our team of experts begin the manufacturing process. This includes precision machining and assembly according to the design documents, requirements and agreed upon contract details.',
+    imageUrl: '/process/04_production.webp',
+    longContent: '',
+  },
+  {
+    id: 5,
+    heading: 'QUALITY CONTROL',
+    content: 'The quality control and assurance process is conducted after completion of each part to ensure the project meets the tolerance and quality requirements. This includes a final inspection and testing.',
+    imageUrl: '/process/05_quality_control.webp',
+    longContent: '',
+  },
+  {
+    id: 6,
+    heading: 'SHIPMENT',
+    content: 'Custom designed packaging is arranged for each completed manufacturing run to ensure that the your project is delivered safe and secure to your location. Photos and tracking information are always provided. ',
+    imageUrl: '/process/06_shipment.webp',
+    longContent: '',
+  },
+];
+
+export default processes;
