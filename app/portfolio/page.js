4196ef80c969c80153ebdeef213ed3d2fa4dc910
Devon Kiss <faktsonly@gmail.com>
2024-11-02 11:51:34 -0500

    Additional context and in introductory paragraph has been provided for the Portfolio/Recent Work view at /portfolio.

--- app/portfolio/page.js
+++ app/portfolio/page.js
@@ -8,8 +8,14 @@ export default function PortfolioPage() {
         <div className="mt-24 text-white pt-0 p-8">
           <section className="mb-12">
             <h1 className="text-4xl font-bold text-center">
-              Recent Work
+              Our Recent Work
             </h1>
+          </section>
+
+          <section>
+            <p className="text-gray-300">
+              Please find our most recent work below. Press on an image and project title for more information. All of the displayed parts are for sale. Each product has a link to it's the product details page where you can find all applicable information. Should you need more information do not hesitate to <a href="/contact-us">contact us</a> for additional information regaring fitment, use, and other inquries.
+            </p>
           </section>
         </div>
       </div>
