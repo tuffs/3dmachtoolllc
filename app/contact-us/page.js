42ad8be664081c2a83b960615b4e1bbda2d818eb
Devon Kiss <faktsonly@gmail.com>
2024-11-03 16:55:17 -0600

    Changes to sizing of the successful message prompt for mobile and desktop browsers alike.

--- app/contact-us/page.js
+++ app/contact-us/page.js
@@ -69,7 +69,7 @@ export default function ContactUsPage() {
             </p>
 
             {state.success ? (
-              <div className="mt-8 mx-auto p-12 bg-gray-900 text-white rounded-xl w-[400px]">
+              <div className="mt-8 mx-auto p-12 bg-gray-900 text-white rounded-xl w-[90%] md:w-[400px]">
                 <h1 className="text-5xl font-bold text-center mb-3" style={{ textShadow: "1px 1px rgba(255,255,255,.25)" }}>✅</h1>
                 <h2 className="text-2xl font-bold text-center" style={{ textShadow: "1px 1px rgba(0,0,0,.4)" }}>Message Sent!</h2>
                 <p className="text-center" style={{ textShadow: "1px 1px rgba(0,0,0,.24)" }}>We'll get back to you as soon as possible.</p>
