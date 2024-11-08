9d0970731f255fe680867aaa757029ff99dceaea
Devon Kiss <faktsonly@gmail.com>
2024-11-08 05:32:19 -0600

    Added a MenuIcon component to the @/components/ui folder for use with Mobile Navigation from thin widthed browser windows (less than the current *md breakpoint).

---
+++ components/ui/MenuIcon.js
@@ -0,0 +1,12 @@
+const MenuIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" stroke="white" width="33" height="33">
+    <polyline points="0,8 33,8"></polyline>
+    <polyline points="0,16 22,16"></polyline>
+    <polyline points="0,24 33,24"></polyline>
+    <title>
+      Navigation
+    </title>
+  </svg>
+)
+
+export default MenuIcon
