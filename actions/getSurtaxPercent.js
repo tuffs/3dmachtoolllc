161ec60e73b2e28c65b3761671bd8c4ee550d51c
Devon Kiss <devon@3dmandt.com>
2025-06-19 04:32:51 -0500

    Using findUnique method instead of find to appropriately utilize Prisma ORM in my server action for getting surtax percentages.

--- actions/getSurtaxPercent.js
+++ actions/getSurtaxPercent.js
@@ -5,7 +5,7 @@ import prisma from '@/prisma/database';
 export async function getSurtaxPercent(zipCode) {
   if (!zipCode || zipCode === "") return 0.0;
 
-  return await prisma.floridaSalesTax.find({
+  return await prisma.floridaSalesTax.findUnique({
     where: { zipCode },
     select: {
       rate: true
