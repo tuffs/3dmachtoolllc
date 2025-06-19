54ec9ea23a63d69b16ea4bfde6a77a23a53797f9
Devon Kiss <devon@3dmandt.com>
2025-06-19 16:28:49 -0500

    Changes to the surtax action for error handling.

--- actions/getSurtaxPercent.js
+++ actions/getSurtaxPercent.js
@@ -1,9 +1,13 @@
 'use server';
 
 import prisma from '@/prisma/database';
+import validator from 'validator';
 
 export async function getSurtaxPercent(zipCode) {
-  if (!zipCode || zipCode === "") return 0.0;
+  // Validate the input
+  if (!zipCode) {
+    return { error: 'Zip code is required', rate: 0.0 };
+  }
 
   return await prisma.floridaSalesTax.findUnique({
     where: { zipCode },
